efd13a4b6091ec936bb588ea54444cdf183d8565
houzhihuil <houzhihuil@gmail.com>
2023-08-10 12:29:42 -0400

    Add CORS

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+
+//
+
+// Add this middleware to set up CORS headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend's URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.listen(3000);
  
 // Define a route to render the produit.json file

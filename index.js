4f59800b5cf455b165a85b4159b4c0681256d3a1
houzhihuil <houzhihuil@gmail.com>
2023-08-12 21:00:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.set("views", "./views");
 
 // Add this middleware to set up CORS headers
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://main--neon-cheesecake-5d962c.netlify.app/', '*', 'https://my-react-app-virid-psi.vercel.app/' ); // Replace with your frontend's URL
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://my-react-app2023.netlify.app/', '*', 'https://my-react-app-virid-psi.vercel.app/' ); // Replace with your frontend's URL
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
